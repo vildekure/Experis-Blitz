9fd73c796f6b6595d111176e1f49ecf10903b5e6
OzKara <ozankarapri@gmail.com>
2023-11-24 09:03:18 +0100

    changed cell type for email to correct type

--- javascript/admin-dashboard.js
+++ javascript/admin-dashboard.js
@@ -1,50 +1,49 @@
-
 function saveScore(username, number, email, score) {
-    const newEntry = { username, number, email, score };
-    const scoreBoard = JSON.parse(localStorage.getItem('scoreBoard')) || [];
-    scoreBoard.push(newEntry);
-    localStorage.setItem('scoreBoard', JSON.stringify(scoreBoard));
+	const newEntry = { username, number, email, score };
+	const scoreBoard = JSON.parse(localStorage.getItem("scoreBoard")) || [];
+	scoreBoard.push(newEntry);
+	localStorage.setItem("scoreBoard", JSON.stringify(scoreBoard));
 }
 
-window.onload = function() {
-    const scoreBoard = JSON.parse(localStorage.getItem('scoreBoard')) || [];
-    const tableBody = document.querySelector('#adminContent table tbody');
+window.onload = function () {
+	const scoreBoard = JSON.parse(localStorage.getItem("scoreBoard")) || [];
+	const tableBody = document.querySelector("#adminContent table tbody");
 
-    scoreBoard.sort((a, b) => b.score - a.score);
+	scoreBoard.sort((a, b) => b.score - a.score);
 
-    scoreBoard.forEach((entry, index) => {
-        let row = tableBody.insertRow();
-        let cellRank = row.insertCell(0);
-        let cellName = row.insertCell(1);
-        let cellNumber = row.insertCell(2);
-        let cellEmail = row.insertCell(3);
-        let cellScore = row.insertCell(4);
+	scoreBoard.forEach((entry, index) => {
+		let row = tableBody.insertRow();
+		let cellRank = row.insertCell(0);
+		let cellName = row.insertCell(1);
+		let cellNumber = row.insertCell(2);
+		let cellEmail = row.insertCell(3);
+		let cellScore = row.insertCell(4);
 
-        cellRank.innerHTML = index < 3 ? `<span class="medal">${['🥇', '🥈', '🥉'][index]}</span>` : '';
-        cellName.textContent = entry.username;
-        cellNumber.textContent = entry.number;
-        cellEmail.email = entry.email;
-        cellScore.textContent = entry.score;
-    });
+		cellRank.innerHTML =
+			index < 3
+				? `<span class="medal">${["🥇", "🥈", "🥉"][index]}</span>`
+				: "";
+		cellName.textContent = entry.username;
+		cellNumber.textContent = entry.number;
+		cellEmail.textContent = entry.email;
+		cellScore.textContent = entry.score;
+	});
 };
 
 function deleteHistory() {
-    localStorage.removeItem('scoreBoard');
-    const tableBody = document.querySelector('#adminContent table tbody');
-    tableBody.innerHTML = '';
+	localStorage.removeItem("scoreBoard");
+	const tableBody = document.querySelector("#adminContent table tbody");
+	tableBody.innerHTML = "";
 }
-
 
 function logout() {
-    window.location.href = '/';
+	window.location.href = "/";
 }
 
+document
+	.getElementById("deleteButton")
+	.addEventListener("click", deleteHistory);
 
-document.getElementById('deleteButton').addEventListener('click', deleteHistory);
-
-    document.getElementById('logoutButton').addEventListener('click', function() {
-        window.location.href = '../pages/homepage.html'; // Endrer URL til hjemmesiden
-    });
-
-
-
+document.getElementById("logoutButton").addEventListener("click", function () {
+	window.location.href = "../pages/homepage.html"; // Endrer URL til hjemmesiden
+});
